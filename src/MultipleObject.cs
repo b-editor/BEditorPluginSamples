b06851f369233016b20b4732859b157d784f95dd
indigo-san <lugongdao07@gmail.com>
2021-10-23 18:12:21 +0900

    多重オブジェクトのサンプルを作成

---
+++ src/MultipleObject.cs
@@ -0,0 +1,105 @@
+﻿using BEditor.Data;
+using BEditor.Data.Primitive;
+using BEditor.Data.Property;
+using BEditor.Drawing;
+using BEditor.Drawing.Pixel;
+using BEditor.Graphics;
+using BEditor.Media;
+
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace BEditorPluginSamples
+{
+    public class MultipleObject : ImageObject
+    {
+        public override string Name => "多重画像";
+
+        public override IEnumerable<PropertyElement> GetProperties()
+        {
+            // プロパティを持たない場合
+            // "yield break;" or "return Array.Empty<PropertyElement>();" or "return Enumerable.Empty<PropertyElement>();"
+
+            yield return Coordinate;
+            yield return Scale;
+            yield return Blend;
+            yield return Rotate;
+            yield return Material;
+        }
+
+        protected override Image<BGRA32> OnRender(EffectApplyArgs args)
+        {
+            // void OnRender(EffectApplyArgs<IEnumerable<Texture>> args) を実装していても呼び出される可能性があるので
+            // 例外は投げないでください
+            return new Image<BGRA32>(1, 1, (BGRA32)default);
+        }
+
+        protected override void OnRender(EffectApplyArgs<IEnumerable<Texture>> args)
+        {
+            args.Value = Render(args.Frame);
+        }
+
+        private IEnumerable<Texture> Render(Frame frame)
+        {
+            // サイズ
+            var size = 100;
+            var half = size / 2;
+            var quarter = half / 2;
+
+            // 円を作成
+            var circle = Image.Ellipse(size, size, size, Colors.White);
+            // 左上
+            using var upperLeft = circle[new Rectangle(0, 0, half, half)];
+            // 右上
+            using var upperRight = circle[new Rectangle(half, 0, half, half)];
+            // 左下
+            using var lowerLeft = circle[new Rectangle(0, half, half, half)];
+            // 右下
+            using var lowerRight = circle[new Rectangle(half, half, half, half)];
+
+            // 右下に移動
+            yield return CreateFromImage(upperLeft, frame, new(quarter, -quarter, 0));
+
+            // 左下に移動
+            yield return CreateFromImage(upperRight, frame, new(-quarter, -quarter, 0));
+
+            // 右上に移動
+            yield return CreateFromImage(lowerLeft, frame, new(quarter, quarter, 0));
+
+            // 左上に移動
+            yield return CreateFromImage(lowerRight, frame, new(-quarter, quarter, 0));
+        }
+
+        // 画像からテクスチャを作成
+        private Texture CreateFromImage(Image<BGRA32> image, Frame frame, Vector3 rel)
+        {
+            var texture = Texture.FromImage(image);
+
+            // Materialを設定
+            var ambient = Material.Ambient[frame];
+            var diffuse = Material.Diffuse[frame];
+            var specular = Material.Specular[frame];
+            var shininess = Material.Shininess[frame];
+            texture.Material = new(ambient, diffuse, specular, shininess);
+
+            // Blend を設定
+            var alpha = Blend.Opacity[frame] / 100f;
+            var color = Blend.Color[frame];
+            color.A = (byte)(color.A * alpha);
+            texture.Color = color;
+            texture.BlendMode = (BlendMode)Blend.BlendType.Index;
+
+            // Transformを設定
+            texture.Transform += GetTransform(frame);
+
+            // Transformはstructなので、
+            // "texture.Transform.Relative = rel;"
+            // はできない
+            var transform = texture.Transform;
+            transform.Relative = rel;
+            texture.Transform = transform;
+
+            return texture;
+        }
+    }
+}
---
+++ src/Plugin.cs
@@ -0,0 +1,78 @@
+﻿
+using BEditor;
+using BEditor.Data;
+using BEditor.Drawing;
+using BEditor.Plugin;
+
+using OpenCvSharp;
+
+using System;
+using System.ComponentModel;
+using System.IO;
+
+using static BEditorPluginSamples.PluginSettings;
+
+namespace BEditorPluginSamples
+{
+    public class Plugin : PluginObject
+    {
+        private SettingRecord? _settings;
+
+        public Plugin(PluginConfig config)
+            : base(config)
+        {
+        }
+
+        public override string PluginName => "BEditorPluginSamples";
+
+        public override string Description => "プラグインのサンプル";
+
+        public override Guid Id { get; } = Guid.Parse("{EBD7CB04-3428-49AF-A85C-5FE7197C6E44}");
+
+        public override SettingRecord Settings
+        {
+            get
+            {
+                if (_settings == null)
+                {
+                    // 設定がnullの場合ファイルから読み込む、
+                    // SettingRecord.LoadFromからnullが返ってきたらデフォルトの設定をNewする。
+                    _settings = SettingRecord.LoadFrom<PluginSettings>(Path.Combine(BaseDirectory, "settings.json"))
+                        ?? new PluginSettings("文字列を入力", 100, Numbers.One);
+                }
+
+                return _settings;
+            }
+            set
+            {
+                _settings = value;
+                // 設定を保存
+                _settings.Save(Path.Combine(BaseDirectory, "settings.json"));
+            }
+        }
+
+        public static void Register()
+        {
+            PluginBuilder.Configure<Plugin>()
+                .With(ObjectMetadata.Create<MultipleObject>("多重画像"))
+                .Register();
+        }
+    }
+
+    public record PluginSettings(
+        [property: DisplayName("文字列")]
+        string String,
+        [property: DisplayName("数値")]
+        int Number,
+        [property: DisplayName("列挙型")]
+        Numbers Enum)
+        : SettingRecord
+    {
+        public enum Numbers
+        {
+            One,
+            Two,
+            Three,
+        }
+    }
+}
